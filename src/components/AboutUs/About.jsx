90eb5495f140a0d0d06f046072082c957e88c714
Raj <rajsrivastav0603@gmail.com>
2022-09-17 15:19:51 +0530

    About page

---
+++ src/components/AboutUs/About.jsx
@@ -0,0 +1,20 @@
+function About() {
+    return (
+        <>
+            <div class="heading">
+                <h1>
+                    About us
+                </h1>
+            </div>
+            <div class="info">
+                <img src="../../assets/About/aboutImg.svg" alt="was not able to load" width="500px"/>
+                    <p>
+                        Majority  of the working-class population of the nation ranging from technical level jobs to white collar jobs struggle with proper styling and representation of their skills through the resume, so we build this application to make that job easier and to provide all styling formats thereby giving the best results.
+                        People find it difficult to convey their skills and knowledge through proper wording while creating such important documents so a proper platform is necessary to resolve all such problems.
+                    </p>
+            </div>
+        </>
+    )
+}
+
+export default About
