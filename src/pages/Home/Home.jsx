08e5d1bc83744e423765cabd2980aa5fe833e3d1
Raj <rajsrivastav0603@gmail.com>
2022-09-18 17:28:03 +0530

    Personal Info Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,8 @@ import React, {useState} from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route , Router } from "react-router-dom";
 // import Mid from "../../components/Mid/Mid";
-import Skills from "../../components/Skills/Skills"
+// import Skills from "../../components/Skills/Skills"
+import PersonalInfo from "../../components/PersonalInfo/PersonalInfo";
 
 
 export default function Home() {
@@ -12,7 +13,9 @@ export default function Home() {
     <>
     {/* <Navbar /> */}
 
-    <Skills />
+    {/* <Skills /> */}
+
+    <PersonalInfo/>
    
     
     </>
