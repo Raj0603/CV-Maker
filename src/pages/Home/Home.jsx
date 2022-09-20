6db2882f06f3109263dda320f332a1d97230c63e
Raj <rajsrivastav0603@gmail.com>
2022-09-20 22:14:45 +0530

    contact

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import React, {useState} from "react";
 
 import Navbar from "../../components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route , Router } from "react-router-dom";
-import Mid from "../../components/Mid/Mid";
+// import Mid from "../../components/Mid/Mid";
 import Contact from "../Contact/Contact";
 
 
