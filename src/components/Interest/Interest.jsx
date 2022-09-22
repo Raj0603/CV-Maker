0bf1419e4ec88818330ee2cc004d4aacad04e370
Raj <rajsrivastav0603@gmail.com>
2022-09-22 16:42:54 +0530

    skills update

--- src/components/Interest/Interest.jsx
+++ src/components/Interest/Interest.jsx
@@ -5,75 +5,79 @@ import "../All Css/Input.css"
 function Interest() {
     return (
         <>
-        
-            <div className="heading">
-                Enter Your Interests
-            </div>
-            <div className="interest-container">
-                <div className="float-child-l">
 
-                    <div className="temp">
-
-                    </div>
+            <div className="in-main">
+                <div className="in-heading">
+                    Enter Your Interests
                 </div>
-                <div className="float-child-r">
-                    <div className="interest-data">
+                <div className="interest-container">
+                    <div className="float-child-l">
+                        <div className="in-suggestion">
+                            <span className="interest-titles">Suggestions</span>
+                            <div className="suggestion-box">
 
-                        <span className="interest-titles" id="interest-name">Achievement</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Achievement in your field</span>
-                        </label>
-                        <span className="interest-titles">Achievement</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Achievement in any other field</span>
-                        </label>
-                        <span className="interest-titles">Achievement</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Achievement in any other field</span>
-                        </label>
-                        <span className="interest-titles" id="interest-name">Interest/Hobbies</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Eg: Music, Dance</span>
-                        </label>
-                        <span className="interest-titles" id="interest-name">Interest/Hobbies</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Eg: Music, Dance</span>
-                        </label>
-                        <span className="interest-titles" id="interest-name">Interest/Hobbies</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Eg: Music, Dance</span>
-                        </label>
-                        {/* <button className="save-changes">Save</button> */}
-                        <br />
-                        <button type="" class="animated-button1" id="save">
-                            <span className="span"></span>
-                            <span className="span"></span>
-                             Save
-                            <span className="span"></span>
-                            <span className="span"></span>
-                        </button>
-                    </div>
-
-                    <div className="suggestion">
-                        <span className="interest-titles">Suggestions</span>
-                        <div className="suggestion-box">
+                            </div>
+                            <button type="" class="animated-button1" id="save">
+                                <span className="span"></span>
+                                <span className="span"></span>
+                                Save
+                                <span className="span"></span>
+                                <span className="span"></span>
+                            </button>
 
                         </div>
-                    <button class="animated-button1" id="Next" type="">
-                        <span className="span"></span>
-                        <span className="span"></span>
-                        <span className="span"></span>
-                        <span className="span"></span>
-                        Next
-                    </button>
-                    </div>
 
+                        <div className="in-temp">
+
+                        </div>
+                    </div>
+                    <div className="float-child-r">
+                        <div className="interest-data">
+
+                            <span className="interest-titles" id="interest-name">Achievement</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Achievement in your field</span>
+                            </label>
+                            <span className="interest-titles">Achievement</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Achievement in any other field</span>
+                            </label>
+                            <span className="interest-titles">Achievement</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Achievement in any other field</span>
+                            </label>
+                            <span className="interest-titles">Interest/Hobbies</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Eg: Music, Dance</span>
+                            </label>
+                            <span className="interest-titles">Interest/Hobbies</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Eg: Music, Dance</span>
+                            </label>
+                            <span className="interest-titles">Interest/Hobbies</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Eg: Music, Dance</span>
+                            </label>
+                            {/* <button className="save-changes">Save</button> */}
+                            <br />
+                            <button class="animated-button1" id="Next" type="">
+                                <span className="span"></span>
+                                <span className="span"></span>
+                                <span className="span"></span>
+                                <span className="span"></span>
+                                Next
+                            </button>
+                        </div>
+
+
+
+                    </div>
                 </div>
             </div>
         </>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -6,75 +6,80 @@ import { Link } from "react-router-dom"
 function Skills() {
     return (
         <>
-        
-            <div className="heading">
-                Enter Your Skills and Experirnce
-            </div>
-            <div className="skills-container">
-                <div className="float-child-l">
+            <div className="sk-main">
 
-                    <div className="temp">
-
-                    </div>
+                <div className="sk-heading">
+                    Enter Your Skills and Experience
                 </div>
-                <div className="float-child-r">
-                    <div className="skills-data">
+                <div className="skills-container">
+                    <div className="float-child-l">
+                        <div className="sk-suggestion">
+                            <span className="skills-titles">Suggestions</span>
+                            <div className="suggestion-box">
 
-                        <span className="skills-titles" id="skills-name">Title</span>
-                        {/* <input className="skills-inputs" type="text" placeholder="Enter full Name" /> */}
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Enter skills Title</span>
-                        </label>
-                        <span className="skills-titles">Overview</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Basic details like why, for whom, etc</span>
-                        </label>
-                        <span className="skills-titles">Imp Point</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Functionlity and Creativity of skills</span>
-                        </label>
-                        <span className="skills-titles">Experience</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Skills learned/ Experience gained</span>
-                        </label>
-                        <span className="skills-titles">Deployment Links</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">skills You have Deployed</span>
-                        </label>
-                        <span className="skills-titles">Github</span>
-                        <label class="input">
-                            <input class="input__field" type="text" placeholder=" " />
-                            <span class="input__label">Github Link for skills</span>
-                        </label>
-                        {/* <button className="save-changes">Save</button> */}
-                        <br />
-                        <button type="" class="animated-button1" id="save">
-                            <span className="span"></span>
-                            <span className="span"></span>
-                             Save
-                            <span className="span"></span>
-                            <span className="span"></span>
-                        </button>
-                    </div>
-
-                    <div className="suggestion">
-                        <span className="skills-titles">Suggestions</span>
-                        <div className="suggestion-box">
+                            </div>
+                            <button type="" class="animated-button1" id="save">
+                                <span className="span"></span>
+                                <span className="span"></span>
+                                Save
+                                <span className="span"></span>
+                                <span className="span"></span>
+                            </button>
 
                         </div>
-                    <Link to="./Certification" class="animated-button1" id="Next" type="">
-                        <span className="span"></span>
-                        <span className="span"></span>
-                        <span className="span"></span>
-                        <span className="span"></span>
-                        Next
-                    </Link>
+
+
+                        <div className="sk-temp">
+
+                        </div>
                     </div>
+                    <div className="float-child-r">
+                        <div className="skills-data">
+
+                            <span className="skills-titles" id="skills-name">Title</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Enter skills Title</span>
+                            </label>
+                            <span className="skills-titles">Overview</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Basic details like why, for whom, etc</span>
+                            </label>
+                            <span className="skills-titles">Imp Point</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Functionlity and Creativity of skills</span>
+                            </label>
+                            <span className="skills-titles">Experience</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Skills learned/ Experience gained</span>
+                            </label>
+                            <span className="skills-titles">Deployment Links</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">skills You have Deployed</span>
+                            </label>
+                            <span className="skills-titles">Github</span>
+                            <label class="input">
+                                <input class="input__field" type="text" placeholder=" " />
+                                <span class="input__label">Github Link for skills</span>
+                            </label>
+                            {/* <button className="save-changes">Save</button> */}
+                            <br />
+                            <Link to="./Certification" class="animated-button1" id="Next" type="">
+                                <span className="span"></span>
+                                <span className="span"></span>
+                                <span className="span"></span>
+                                <span className="span"></span>
+                                Next
+                            </Link>
+
+                        </div>
+                    </div>
+
+
 
                 </div>
             </div>
